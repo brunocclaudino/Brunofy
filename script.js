3b354e59c74e84a6de1ead61664d4ff6b42f874d
brunocclaudino <brunocclaudino@gmail.com>
2024-09-02 13:28:57 -0400

    Atualizando os arquivos index, script e style

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const song = document.getElementById('audio');
 const play= document.getElementById('play');
 const next= document.getElementById('next');
 const previous= document.getElementById('previous');
+const currentProgress= document.getElementById('current-progress');
+const progressContainer= document.getElementById('progress-container');
+const shuffleButton= document.getElementById('shuffle');
+
 const soPorUmaNoite = {
     songName: 'Só Por Uma Noite (Ao Vivo)',
     artist:'Charlie Brown Jr.',
@@ -25,7 +29,9 @@ const dontLookBackMeAnger = {
 }
 
 let isPlaying = false;
-const playlist = [soPorUmaNoite,drainYou,dontLookBackMeAnger];
+let isShuffled = false;
+const originalPlaylist = [soPorUmaNoite,drainYou,dontLookBackMeAnger];
+let sortedPlaylist = [...originalPlaylist]
 let index = 0;
 
 
@@ -53,14 +59,14 @@ function playPauseDecider(){
 }
 
 function initializeSong(){
-    cover.src =`images/${playlist[index].cover}.jpeg`;
-    song.src = `songs/${playlist[index].file}.mp3`;
-    songName.innerText = playlist[index].songName;
-    bandName.innerText = playlist[index].artist;
+    cover.src =`images/${sortedPlaylist[index].cover}.jpeg`;
+    song.src = `songs/${sortedPlaylist[index].file}.mp3`;
+    songName.innerText = sortedPlaylist[index].songName;
+    bandName.innerText = sortedPlaylist[index].artist;
 }
 function previousSong(){
     if(index === 0){
-        index = playlist.length - 1;
+        index = sortedPlaylist.length - 1;
     }else{
         index--;
     }
@@ -68,7 +74,7 @@ function previousSong(){
     playSong();
 }
 function nextSong(){
-    if(index === playlist.length - 1){
+    if(index === sortedPlaylist.length - 1){
         index = 0;
     }else{
         index++;
@@ -76,7 +82,46 @@ function nextSong(){
     initializeSong();
     playSong();
 }
+function updateProgessBar(){
+    const barWidth = (song.currentTime/song.duration)*100;
+    currentProgress.style.setProperty('--progress',`${barWidth}%`);
+}
+function jumpTo(event){
+    const width = progressContainer.clientWidth;
+    const clickPosition = event.offsetX;
+    const jumpToTime = (clickPosition/width)*song.duration;
+    song.currentTime = jumpToTime;
+}
+function shuffleArray(preShuffleArray){
+    const size = preShuffleArray.length;
+    let currentIndex = size - 1;
+    
+    while(currentIndex > 0 ){
+        let randomIndex = Math.floor(Math.random() * size);
+        let aux = preShuffleArray[currentIndex];
+        preShuffleArray[currentIndex] = preShuffleArray[randomIndex];
+        preShuffleArray[randomIndex] = aux;
+        currentIndex -= 1;
+    }
+}
+function shuffleMode(){
+    if(isShuffled === false){
+        isShuffled = true;
+        shuffleArray(sortedPlaylist);
+        //console.log('MISTO: [0]'+JSON.stringify(sortedPlaylist[0])+'[1]'+JSON.stringify(sortedPlaylist[1])+'[2]'+JSON.stringify(sortedPlaylist[2]));
+        shuffleButton.classList.add('button-active');
+    }
+    else{
+        isShuffled = false;
+        sortedPlaylist = [...originalPlaylist];
+        shuffleButton.classList.remove('button-active');
+        //console.log('Original: [0]'+JSON.stringify(sortedPlaylist[0])+'[1]'+JSON.stringify(sortedPlaylist[1])+'[2]'+JSON.stringify(sortedPlaylist[2]));
+    }
+}
 initializeSong();
 play.addEventListener('click',playPauseDecider);
 previous.addEventListener('click',previousSong);
 next.addEventListener('click',nextSong);
+song.addEventListener('timeupdate', updateProgessBar);
+progressContainer.addEventListener('click', jumpTo);
+shuffleButton.addEventListener('click', shuffleMode);
